42b6bab28d9b1bf456bc22235543490f36e99520
Chinmay Kumar Panda <126417086+ChinmayKumarPanda@users.noreply.github.com>
2025-07-22 16:19:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello from Node.js on Amazon Linux 26!</h1>');
+  res.end('<h1>Hello from Node.js on Amazon Linux 21!</h1>');
 });
 
 server.listen(PORT, () => {

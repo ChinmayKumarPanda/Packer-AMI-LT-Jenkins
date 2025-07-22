31e17a8c69723ff1b0315ef04700b6d4a8673bef
root <root@ip-172-31-37-171.ap-south-1.compute.internal>
2025-07-22 09:28:07 +0000

    Add index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Hello from Node.js on Amazon Linux 25!</h1>');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}/`);
+});
+
